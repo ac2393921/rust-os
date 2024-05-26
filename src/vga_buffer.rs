5040bda2c32bbf3df045aad83518504f5df53841
ac2393921 <ac2393921@gmail.com>
2024-05-27 00:58:49 +0900

    feat: フォーマットマクロを追加

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use volatile::Volatile;
 
 #[allow(dead_code)]
@@ -74,7 +75,7 @@ impl Writer {
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                // コンパイラが書き込みを最適化して取り除いてしまわないことが保証
+                // volatileでコンパイラが書き込みを最適化して取り除いてしまわないことを保証
                 self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
@@ -103,7 +104,15 @@ impl Writer {
     }
 }
 
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
@@ -112,5 +121,5 @@ pub fn print_something() {
 
     writer.write_byte(b'H');
     writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
 }
