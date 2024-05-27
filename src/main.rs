34338ad3da1479641b27f44e67b927eb31bc7061
ac2393921 <ac2393921@gmail.com>
2024-05-27 23:04:20 +0900

    feat: シリアルポート経由で出力するように変更

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
@@ -26,7 +27,7 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -35,9 +36,9 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... "); // "些末なアサーション……"
+    serial_print!("trivial assertion... "); // "些末なアサーション……"
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
